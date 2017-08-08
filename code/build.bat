45da1ca64125b729de49caa45d2301fff424e476
SaintForge <mr.spartan-117@yandex.ru>
2017-08-09 00:28:59 +0300

    Added day 10(QueryPerformanceQuery and RDTSC)

--- code/build.bat
+++ code/build.bat
@@ -2,6 +2,6 @@
 
 mkdir ..\build
 pushd ..\build
-call "D:\Visual Studio\VC\vcvarsall.bat" x86
+call "D:\Visual Studio\VC\vcvarsall.bat" x64
 cl -Zi w:\handmade\code\win32_handmade.cpp user32.lib Gdi32.lib
 popd

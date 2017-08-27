1db8cb962ccf2a0466cb486cf5694c31a392d836
SaintForge <mr.spartan-117@yandex.ru>
2017-08-27 16:43:02 +0300

    Day 12 - Platform-independent Sound Output

--- code/build.bat
+++ code/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-mkdir ..\build
+IF NOT EXIST ..\build ..\build
 pushd ..\build
 call "D:\Visual Studio\VC\vcvarsall.bat" x64
 cl -Zi w:\handmade\code\win32_handmade.cpp user32.lib Gdi32.lib
